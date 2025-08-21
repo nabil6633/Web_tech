d35840a969a39a66082113f8eb83c734fae40009
22-49909-3@student.aiub.edu <22-49909-3@student.aiub.edu>
2025-08-22 04:08:07 +0600

    mid

---
+++ JavaScript.js
@@ -0,0 +1,8 @@
+//document.addEventListener("load", display());
+document.getElementById("btn1").addEventListener("click", display);
+
+function display(){
+    
+    let div1 = document.getElementById("div1");
+    div1.style.backgroundColor = "lightblue";
+}
